9c02318356c5ae01e8af63e470695920faa4bd9d
g-barrett <barrettg516@gmail.com>
2023-08-24 02:28:12 -0500

    finished function for pass generator and readme

--- Assets/script.js
+++ Assets/script.js
@@ -14,7 +14,6 @@
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page
 
-// Assignment code here
 function generatePassword() {
   var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()`~.-_/;:[]{}ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var password = "";
@@ -26,25 +25,83 @@ function generatePassword() {
   console.log(numeric);
   var specialChar = window.confirm("Would you like to include special characters?");
   console.log(specialChar);
-  if (
-    (upperCase === true) ||
-    (numeric === true) ||
-    (specialChar === true)) {
+  if ( //all lowercase
+    (upperCase === false) &&
+    (numeric === false) &&
+    (specialChar === false)) {
+      var characters2 = characters.substring(10, 36);
+      console.log(characters2);
+      for (var i = 1; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * characters2.length + 1);
+        password += characters2.charAt(randomNumber);
+        console.log(password);
+      }
+    } else if (  //lowercase, numbers, and special characters
+      (upperCase === false) &&
+      (numeric === true) &&
+      (specialChar === true)) {
+        var characters3 = characters.substring(0, 58);
+        console.log(characters3);
+        for (var i = 1; i <= passwordLength; i++) {
+          var randomNumber = Math.floor(Math.random() * characters3.length + 1);
+          password += characters3.charAt(randomNumber);
+          console.log(password);
+      }
+    } else if ( //lowercase and special characters
+      (upperCase === false) &&
+      (numeric === false) &&
+      (specialChar === true)) {
+        var characters4 = characters.substring(10, 58);
+        console.log(characters4)
+        for (var i = 1; i <= passwordLength; i++) {
+          var randomNumber = Math.floor(Math.random() * characters4.length + 1);
+          password += characters4.charAt(randomNumber);
+        }
+    } else if ( //lowercase, uppercase, and special characters
+      (upperCase === true) &&
+      (numeric === false) &&
+      (specialChar === true)) {
+        var characters5 = characters.substring(10);
+        console.log(characters5)
+        for (var i = 1; i <= passwordLength; i++) {
+          var randomNumber = Math.floor(Math.random() * characters5.length + 1);
+          password += characters5.charAt(randomNumber);
+        }
+    } else if (  //lowercase and numbers
+      (upperCase === false) &&
+      (numeric === true) &&
+      (specialChar === false)) {
+        var characters6 = characters.substring(0, 36);
+        console.log(characters6)
+        for (var i = 1; i <= passwordLength; i++) {
+          var randomNumber = Math.floor(Math.random() * characters6.length + 1);
+          password += characters6.charAt(randomNumber);
+        }
+    } else if (  //lowercase, uppercase, and numbers
+      (upperCase === true) &&
+      (numeric === true) &&
+      (specialChar === false)) {
+        var characters7 = characters.substring(0, 36) + characters.substring(58);
+        console.log(characters7)
+        for (var i = 1; i <= passwordLength; i++) {
+          var randomNumber = Math.floor(Math.random() * characters7.length + 1);
+          password += characters7.charAt(randomNumber);
+        }
+    } else if (  //lowercase and uppercase
+    (upperCase === true) &&
+    (numeric === false) &&
+    (specialChar === false)) {
+      var characters8 = characters.substring(10, 36) + characters.substring(58);
+      console.log(characters8)
+      for (var i = 1; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * characters8.length + 1);
+        password += characters8.charAt(randomNumber);
+      }
+    } else { //all criteria
       for (var i = 1; i <= passwordLength; i++) {
         var randomNumber = Math.floor(Math.random() * characters.length + 1);
         password += characters.charAt(randomNumber);
         console.log(password);
-      }
-    } else if ( //this is not working currently
-      (upperCase === false) ||
-      (numeric === true) ||
-      (specialChar === true)) {
-        var characters2 = characters.substring(0, 58);
-        console.log(characters2);
-        for (var i = 1; i <= passwordLength; i++) {
-          var randomNumber = Math.floor(Math.random() * characters2.length + 1);
-          password += characters2.charAt(randomNumber);
-          console.log(password);
       }
     }
     return password;
